78e8f79d8c6dccb2d032f309ec0375f46b44f01e
juliexsun <152749775+juliexsun@users.noreply.github.com>
2024-03-31 23:39:09 -0400

    Update main.py
    fixed jsonl wrong file issue

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from read_data import *
 
 if __name__ == "__main__":
     review_data = load_clean_review_data("All_Beauty.jsonl")
-    product_data = load_clean_product_data('meta_All_Beauty.json')
+    product_data = load_clean_product_data("meta_All_Beauty.jsonl")
