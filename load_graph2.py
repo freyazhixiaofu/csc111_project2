54a168d03dd65100904f2adbe236b31f0955dd67
freyazhixiaofu <149891141+freyazhixiaofu@users.noreply.github.com>
2024-04-04 14:30:22 -0400

    graph2_pytaok

--- load_graph2.py
+++ load_graph2.py
@@ -1,10 +1,10 @@
 """This python module contians the necessary classes and functions that are required to
 loads our second graph, a product to product graph where the connection between the products
-is being displayed.
+is being displayed."""
 
-"""
 from __future__ import annotations
 from typing import Any, Union
+import networkx as nx
 from graph_construction import Graph
 
 
@@ -43,7 +43,7 @@ class WeightedGraph:
       If an edge is added between two products, that means there is a connection between the two, the weight of the edge
       between any two procuts represents how strong their connection is. """
 
-    _vertices: dict[Any, _WeightedVertex]
+    _vertices: dict[str, _WeightedVertex]  # changed Any to str
 
     def __init__(self) -> None:
         """Initialize an empty graph with no vertices or edges."""
@@ -79,10 +79,65 @@ class WeightedGraph:
         """return the dictionary of all products in weighted graph"""
         return self._vertices
 
+    def get_weight(self, v_p1: _WeightedVertex, v_p2: _WeightedVertex) -> float:
+        """given the names of the two products, return the weight on their edge."""
+        return v_p1.neighbours[v_p2]
+
+    def adjacent(self, p1: str, p2: str) -> bool:
+        """If two products are adjacent in the graph"""
+        if not (p1 in self._vertices and p2 in self._vertices):
+            raise ValueError
+        v1 = self._vertices[p1]
+        return any(v2.name == p2 for v2 in v1.neighbours)
+
+    def get_all_vertices(self) -> set[str]:
+        """A set of all the product names in the graph"""
+        return {v.name for v in self._vertices.values()}
+
+    def to_networkx(self, max_vertices: int = 5000) -> nx.Graph:
+        """Convert this graph into a networkx Graph.
+
+        max_vertices specifies the maximum number of vertices that can appear in the graph.
+        (This is necessary to limit the visualization output for large graphs.)
+
+        Note that this method is provided for you, and you shouldn't change it.
+        """
+        graph_nx = nx.Graph()
+        for v in self._vertices.values():
+            graph_nx.add_node(v.name, kind=v.name)
+
+            for u in v.neighbours.keys():
+                if graph_nx.number_of_nodes() < max_vertices:
+                    graph_nx.add_node(u.name, kind=u.name)
+
+                if u.name in graph_nx.nodes:
+                    graph_nx.add_edge(v.name, u.name, weight=v.neighbours[u])
+
+            if graph_nx.number_of_nodes() >= max_vertices:
+                break
+
+        return graph_nx
+
+
+def counting_pairs(lst: list) -> dict[tuple: int]:
+    """Counts the number of occurrences of each pair of products, which represents double of the number of users that
+    purhcased both products, in the list passed in.
+    Returns a dictionary mapping the tuple with the corresponding number of occurrences in the lst passed in.
+    Note that
+
+    lst = [(1, 4), (2, 6), (3, 4), (10, 22), (1, 4), (1, 4), (3, 4), (3, 4), (10, 22), (10, 22), (3, 4)]
+    >>> d = counting_pairs(lst)
+    >>> d
+    {(1, 4): 3, (2, 6): 1, (3, 4): 4, (10, 22): 3}
+    """
+    d = {}
+    for tup in lst:
+        d[tup] = d.setdefault(tup, 0) + 1
+    return d
+
 
 def get_all_pairs(g: Graph) -> list[tuple]:
-    """Returns a list of tuples.
-    We will get the duplicate of all the occurrences of the pairs of the products that are bought by the same person.
+    """We will get the duplicate of all the occurrences of the pairs of the products that are bought by the same person.
     each pair will be represented by a tuple
     Notice that because the first grpah is a bipartite graph, every product's neighbour is a user, and every product's
     neighbour's neighbour is another product bought by the same user. Hence, this function is finding all the
@@ -92,33 +147,26 @@ def get_all_pairs(g: Graph) -> list[tuple]:
 
     A postcondition is that all the tuples in the list must be sorted in alphabetical order.
 
+    >>>
     """
     lst = []
     products = g.get_all_vertices('product')
     for p in products:
-        for user in g.get_vertex(p).neighbours:
-            assert g.adjacent(user.item, p)
-            for also_bought in user.neighbours:
-                pair_list = sorted([p, also_bought.item])
-                assert g.adjacent(user.item, also_bought.item)
-                lst.append((pair_list[0], pair_list[1]))
+        connected_v = get_double_nb(p, g)
+        for product in connected_v:
+            pair_list = sorted([product, p])
+            lst.append((pair_list[0], pair_list[1]))
     return lst
 
 
-def counting_pairs(lst: list) -> dict[tuple: int]:
-    """Counts the number of occurrences of each pair of products, which represents double of the number of users that
-    purhcased both products, in the list passed in.
-    Returns a dictionary mapping the tuple with the corresponding number of occurrences in the lst passed in.
-    Note that
-
-    >>> lst = [(1, 4), (2, 6), (3, 4), (10, 22), (1, 4), (1, 4), (3, 4), (3, 4), (10, 22), (10, 22), (3, 4)]
-    >>> counting_pairs(lst)
-    {(1, 4): 3, (2, 6): 1, (3, 4): 4, (10, 22): 3}
-    """
-    d = {}
-    for tup in lst:
-        d[tup] = d.setdefault(tup, 0) + 1
-    return d
+def get_double_nb(v: str, g: Graph) -> list[str]:
+    """Get neighbour's neighbours that are not itself"""
+    s = []
+    for nb in g.get_vertex(v).neighbours:
+        for nb2 in nb.neighbours:
+            if nb2.item != v:
+                s.append(nb2.item)
+    return s
 
 
 def calculate_weight(rating1: float, rating2: float, time1: float, time2: float, occurrences: int) -> float:
@@ -126,8 +174,8 @@ def calculate_weight(rating1: float, rating2: float, time1: float, time2: float,
     time stamp, and the number of users who have purchased both products.
 
     A formula will be used to compute the weight.
-    # >>> calculate_weight(3, 4, 8232141241332, 8231231434122, 4)
-    # 6
+    >>> calculate_weight(3, 4, 8232141241332, 8231231434122, 4)
+    6
     """
     time_diff = int(pow(10, -10) * (time1 - time2))
     if time_diff == 0:
@@ -161,18 +209,21 @@ def load_graph2(g1: Graph, d: dict[tuple: int]) -> WeightedGraph:
     """
     g2 = WeightedGraph()
     for tup in d:
-        if d[tup] // 2 >= 2:
-            v1 = g1.get_vertex(tup[0])
-            v2 = g1.get_vertex(tup[1])
-            rating1 = sum(v1.all_review) / len(v1.all_review)
-            rating2 = sum(v2.all_review) / len(v2.all_review)
-            time1 = sum(v1.all_time_stamp) / len(v1.all_time_stamp)
-            time2 = sum(v2.all_time_stamp) / len(v2.all_time_stamp)
-            occurrences = d[tup]
-            weight = calculate_weight(rating1, rating2, time1, time2, occurrences)
-            g2.add_vertex(tup[0])
-            g2.add_vertex(tup[1])
-            g2.add_edge(tup[0], tup[1], weight)
+        if d[tup] // 2 >= 1:
+            if d[tup] // 2 >= 3:
+                v1 = g1.get_vertex(tup[0])
+                v2 = g1.get_vertex(tup[1])
+                rating1 = sum(v1.all_review) / len(v1.all_review)
+                rating2 = sum(v2.all_review) / len(v2.all_review)
+                time1 = sum(v1.all_time_stamp) / len(v1.all_time_stamp)
+                time2 = sum(v2.all_time_stamp) / len(v2.all_time_stamp)
+                occurrences = d[tup]
+                weight = calculate_weight(rating1, rating2, time1, time2, occurrences)
+                g2.add_vertex(tup[0])
+                g2.add_vertex(tup[1])
+                g2.add_edge(tup[0], tup[1], weight)
+                # if g2.adjacent(tup[0], tup[1]):
+                #     print(f'...{g2.get_all_vertices()}...lol')
     return g2
 
 
@@ -192,7 +243,6 @@ if __name__ == '__main__':
 
     python_ta.check_all(config={
         'max-line-length': 120,
-        'extra-imports': ['graph_construction'],
-        'allowed-io': ['graph_construction'],
+        'extra-imports': ['networkx', "graph_construction"],
         'max-nested-blocks': 4
     })
